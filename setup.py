97896a76909a30d2b1dd150953988b8a05fd6480
Matthias Voppichler <xmatthias@outlook.com>
2018-02-22 20:56:37 +0100

    add m2r converter for readme

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
 """setup module"""
 from setuptools import setup, find_packages
+import os
 
 from pywnedpasswords import __version__
+
+readme_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'README.md')
+try:
+    from m2r import parse_from_file
+    readme = parse_from_file(readme_file)
+except ImportError:
+    # m2r may not be installed in user environment
+    print("for releases, please install m2r!")
+    with open(readme_file) as f:
+        readme = f.read()
 
 setup(name="pywnedpasswords",
       version=__version__,
       description="Client for HIBP passwords api using K-Anonymity method",
-      long_description=open('README.md').read(),
+      long_description=readme,
       url="http://github.com/xmatthias/pywnedpasswords",
       author="Matthias Voppichler",
       author_email="xmatthias@outlook.com",
