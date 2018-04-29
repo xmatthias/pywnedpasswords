72d69ad78d1dc84cd2a478e41659d95a5e2d544c
Matthias Voppichler <xmatthias@outlook.com>
2018-04-29 13:13:45 +0200

    simplify setup module as pypi now supports markdown
    * requires `setuptools>=38.6.0` and `twine>=1.11.0`

--- setup.py
+++ setup.py
@@ -5,19 +5,14 @@ import os
 from pywnedpasswords import __version__
 
 readme_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'README.md')
-try:
-    from m2r import parse_from_file
-    readme = parse_from_file(readme_file)
-except ImportError:
-    # m2r may not be installed in user environment
-    print("for releases, please install m2r!")
-    with open(readme_file) as f:
+with open(readme_file) as f:
         readme = f.read()
 
 setup(name="pywnedpasswords",
       version=__version__,
       description="Client for HIBP passwords api using K-Anonymity method",
       long_description=readme,
+      long_description_content_type="text/markdown",
       url="http://github.com/xmatthias/pywnedpasswords",
       author="Matthias Voppichler",
       author_email="xmatthias@outlook.com",
